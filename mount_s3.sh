409c18834bfc0f6a86f0568964933bf267bfb1b4
Ignacio-Losada <50157452+Ignacio-Losada@users.noreply.github.com>
2025-04-22 12:54:04 -0700

    Update mount_s3.sh

--- mount_s3.sh
+++ mount_s3.sh
@@ -2,7 +2,6 @@
 BUCKET_NAME=$1
 DIRECTORY=$2
 shift 2
-# Options are passed to mount-s3. Concatenate them from an array into a single string.
 PASSED_OPTIONS=$*
 mkdir -p ${DIRECTORY}
 mount-s3 ${BUCKET_NAME} ${DIRECTORY} ${PASSED_OPTIONS}
