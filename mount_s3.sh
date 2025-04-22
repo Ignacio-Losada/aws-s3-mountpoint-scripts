2a733d6e42b96b4a6df377d01498025f7214286e
Ignacio-Losada <50157452+Ignacio-Losada@users.noreply.github.com>
2025-04-22 12:08:26 -0700

    Add files via upload

---
+++ mount_s3.sh
@@ -0,0 +1,7 @@
+BUCKET_NAME=$1
+DIRECTORY=$2
+shift 2
+# Options are passed to mount-s3. Concatenate them from an array into a single string.
+PASSED_OPTIONS=$*
+mkdir -p ${DIRECTORY}
+mount-s3 ${BUCKET_NAME} ${DIRECTORY} ${PASSED_OPTIONS}
